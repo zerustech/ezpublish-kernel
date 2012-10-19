ee6eed2f61bc78a0c42cdff7736b2090cb719419
Kore Nordmann <mail@kore-nordmann.de>
2012-10-19 14:20:15 +0200

    Implemented: ContenttypeGroup input parser

---
+++ eZ/Publish/Core/REST/Client/Input/Parser/ContentTypeGroup.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * File containing the ContentTypeGroup parser class
+ *
+ * @copyright Copyright (C) 1999-2012 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Publish\Core\REST\Client\Input\Parser;
+use eZ\Publish\Core\REST\Common\Input\ParserTools;
+use eZ\Publish\Core\REST\Client\ContentTypeService;
+
+use eZ\Publish\Core\REST\Common\Input\Parser;
+use eZ\Publish\Core\REST\Common\Input\ParsingDispatcher;
+
+use eZ\Publish\Core\REST\Client\Values;
+
+/**
+ * Parser for ContentTypeGroup
+ */
+class ContentTypeGroup extends Parser
+{
+    /**
+     * @var \eZ\Publish\Core\REST\Common\Input\ParserTools
+     */
+    protected $parserTools;
+
+    /**
+     * @var \eZ\Publish\Core\REST\Client\ContentTypeService
+     */
+    protected $contentTypeService;
+
+    /**
+     * @param \eZ\Publish\Core\REST\Common\Input\ParserTools $parserTools
+     * @param \eZ\Publish\Core\REST\Client\ContentTypeService $contentTypeService
+     */
+    public function __construct( ParserTools $parserTools, ContentTypeService $contentTypeService )
+    {
+        $this->parserTools = $parserTools;
+        $this->contentTypeService = $contentTypeService;
+    }
+
+    /**
+     * Parse input structure
+     *
+     * @param array $data
+     * @param \eZ\Publish\Core\REST\Common\Input\ParsingDispatcher $parsingDispatcher
+     * @return \eZ\Publish\API\Repository\Values\ContentTypeGroup\ContentTypeGroup
+     */
+    public function parse( array $data, ParsingDispatcher $parsingDispatcher )
+    {
+        $creatorId = $this->parserTools->parseObjectElement( $data['Creator'], $parsingDispatcher );
+        $modifierId = $this->parserTools->parseObjectElement( $data['Modifier'], $parsingDispatcher );
+
+        return new Values\ContentType\ContentTypeGroup(
+            array(
+                'id'   => $data['_href'],
+                'identifier' => $data['identifier'],
+                'creationDate' => new \DateTime( $data['created'] ),
+                'modificationDate' => new \DateTime( $data['modified'] ),
+                'creatorId' => $creatorId,
+                'modifierId' => $modifierId,
+            )
+        );
+    }
+}
