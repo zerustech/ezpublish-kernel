b1e9d7b4e0455925965563eb90a086d00ef1d733
Kore Nordmann <mail@kore-nordmann.de>
2013-01-16 13:59:38 +0100

    Removed useless int padding

--- eZ/Publish/Core/Persistence/SqlNg/schema/schema.mysql.sql
+++ eZ/Publish/Core/Persistence/SqlNg/schema/schema.mysql.sql
@@ -6,7 +6,7 @@ SET foreign_key_checks = 0;
 --
 DROP TABLE IF EXISTS ezcontent_language;
 CREATE TABLE ezcontent_language (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `language_code` VARCHAR(20) NOT NULL DEFAULT '',
     `name` VARCHAR(255) NOT NULL DEFAULT '',
     `is_enabled` INT(1) NOT NULL DEFAULT 0,
@@ -19,22 +19,22 @@ CREATE TABLE ezcontent_language (
 --
 DROP TABLE IF EXISTS ezcontenttype;
 CREATE TABLE ezcontenttype (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(50) NOT NULL DEFAULT '',
-    `initial_language_id` INT(11) NOT NULL DEFAULT '0',
-    `always_available` INT(11) NOT NULL DEFAULT '0',
+    `initial_language_id` INT NOT NULL DEFAULT '0',
+    `always_available` INT NOT NULL DEFAULT '0',
     `contentobject_name` VARCHAR(255) DEFAULT NULL,
-    `is_container` INT(11) NOT NULL DEFAULT '0',
-    `created` INT(11) NOT NULL DEFAULT '0',
-    `creator_id` INT(11) NOT NULL DEFAULT '0',
-    `modified` INT(11) NOT NULL DEFAULT '0',
-    `modifier_id` INT(11) NOT NULL DEFAULT '0',
+    `is_container` INT NOT NULL DEFAULT '0',
+    `created` INT NOT NULL DEFAULT '0',
+    `creator_id` INT NOT NULL DEFAULT '0',
+    `modified` INT NOT NULL DEFAULT '0',
+    `modifier_id` INT NOT NULL DEFAULT '0',
     `remote_id` VARCHAR(100) NOT NULL DEFAULT '',
     `description_list` LONGTEXT,
     `name_list` LONGTEXT,
-    `sort_field` INT(11) NOT NULL DEFAULT '1',
-    `sort_order` INT(11) NOT NULL DEFAULT '1',
-    `status` INT(11) NOT NULL DEFAULT '0',
+    `sort_field` INT NOT NULL DEFAULT '1',
+    `sort_order` INT NOT NULL DEFAULT '1',
+    `status` INT NOT NULL DEFAULT '0',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id, status),
     FOREIGN KEY (initial_language_id) REFERENCES ezcontent_language(id) ON DELETE RESTRICT,
@@ -44,17 +44,17 @@ CREATE TABLE ezcontenttype (
 
 DROP TABLE IF EXISTS ezcontenttype_field;
 CREATE TABLE ezcontenttype_field (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(50) NOT NULL DEFAULT '',
-    `status` INT(11) NOT NULL DEFAULT '0',
+    `status` INT NOT NULL DEFAULT '0',
     `field_group` VARCHAR(25) NOT NULL DEFAULT '',
-    `contenttype_id` INT(11) NOT NULL DEFAULT '0',
-    `can_translate` INT(11) DEFAULT '1',
+    `contenttype_id` INT NOT NULL DEFAULT '0',
+    `can_translate` INT DEFAULT '1',
     `type_string` VARCHAR(50) NOT NULL DEFAULT '',
-    `is_information_collector` INT(11) NOT NULL DEFAULT '0',
-    `is_required` INT(11) NOT NULL DEFAULT '0',
-    `is_searchable` INT(11) NOT NULL DEFAULT '0',
-    `placement` INT(11) NOT NULL DEFAULT '0',
+    `is_information_collector` INT NOT NULL DEFAULT '0',
+    `is_required` INT NOT NULL DEFAULT '0',
+    `is_searchable` INT NOT NULL DEFAULT '0',
+    `placement` INT NOT NULL DEFAULT '0',
     `description_list` LONGTEXT,
     `name_list` LONGTEXT NOT NULL,
     `constraints` LONGTEXT,
@@ -66,12 +66,12 @@ CREATE TABLE ezcontenttype_field (
 
 DROP TABLE IF EXISTS ezcontenttype_group;
 CREATE TABLE ezcontenttype_group (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(255) DEFAULT NULL,
-    `created` INT(11) NOT NULL DEFAULT '0',
-    `creator_id` INT(11) NOT NULL DEFAULT '0',
-    `modified` INT(11) NOT NULL DEFAULT '0',
-    `modifier_id` INT(11) NOT NULL DEFAULT '0',
+    `created` INT NOT NULL DEFAULT '0',
+    `creator_id` INT NOT NULL DEFAULT '0',
+    `modified` INT NOT NULL DEFAULT '0',
+    `modifier_id` INT NOT NULL DEFAULT '0',
     `name_list` LONGTEXT,
     `description_list` LONGTEXT,
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -82,9 +82,9 @@ CREATE TABLE ezcontenttype_group (
 
 DROP TABLE IF EXISTS ezcontenttype_group_rel;
 CREATE TABLE ezcontenttype_group_rel (
-    `contenttype_id` INT(11) NOT NULL DEFAULT '0',
-    `group_id` INT(11) NOT NULL DEFAULT '0',
-    `status` INT(11) NOT NULL DEFAULT '0',
+    `contenttype_id` INT NOT NULL DEFAULT '0',
+    `group_id` INT NOT NULL DEFAULT '0',
+    `status` INT NOT NULL DEFAULT '0',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (contenttype_id, group_id, status),
     FOREIGN KEY (contenttype_id) REFERENCES ezcontenttype(id) ON DELETE CASCADE,
@@ -96,18 +96,18 @@ CREATE TABLE ezcontenttype_group_rel (
 --
 DROP TABLE IF EXISTS ezcontent;
 CREATE TABLE ezcontent(
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `contenttype_id` INT(11) NOT NULL DEFAULT '0',
-    `current_version_no` INT(11) DEFAULT NULL,
-    `initial_language_id` INT(11) NOT NULL DEFAULT '0',
-    `language_mask` INT(11) NOT NULL DEFAULT '0',
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `contenttype_id` INT NOT NULL DEFAULT '0',
+    `current_version_no` INT DEFAULT NULL,
+    `initial_language_id` INT NOT NULL DEFAULT '0',
+    `language_mask` INT NOT NULL DEFAULT '0',
     `name` VARCHAR(255) DEFAULT NULL,
-    `owner_id` INT(11) NOT NULL DEFAULT '0', -- Keep this name, or change to modifier_id?
-    `modified` INT(11) NOT NULL DEFAULT '0',
-    `published` INT(11) NOT NULL DEFAULT '0',
+    `owner_id` INT NOT NULL DEFAULT '0', -- Keep this name, or change to modifier_id?
+    `modified` INT NOT NULL DEFAULT '0',
+    `published` INT NOT NULL DEFAULT '0',
     `remote_id` VARCHAR(100) DEFAULT NULL,
-    `section_id` INT(11) NOT NULL DEFAULT '0',
-    `status` INT(11) DEFAULT '0',
+    `section_id` INT NOT NULL DEFAULT '0',
+    `status` INT DEFAULT '0',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     UNIQUE KEY ezcontentobject_remote_id (remote_id),
@@ -119,9 +119,9 @@ CREATE TABLE ezcontent(
 
 DROP TABLE IF EXISTS ezcontentobject_name;
 CREATE TABLE ezcontentobject_name (
-    `content_id` INT(11) NOT NULL DEFAULT '0',
-    `content_version_no` INT(11) NOT NULL DEFAULT '0',
-    `language_id` INT(11) NOT NULL DEFAULT '0',
+    `content_id` INT NOT NULL DEFAULT '0',
+    `content_version_no` INT NOT NULL DEFAULT '0',
+    `language_id` INT NOT NULL DEFAULT '0',
     `content_translation` VARCHAR(20) NOT NULL DEFAULT '',
     `name` VARCHAR(255) DEFAULT NULL,
     `real_translation` VARCHAR(20) DEFAULT NULL,
@@ -133,13 +133,13 @@ CREATE TABLE ezcontentobject_name (
 
 DROP TABLE IF EXISTS ezcontentversion;
 CREATE TABLE ezcontentversion (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `content_id` INT(11) DEFAULT NULL,
-    `version_no` INT(11) NOT NULL DEFAULT '0',
-    `creator_id` INT(11) NOT NULL DEFAULT '0',
-    `created` INT(11) NOT NULL DEFAULT '0',
-    `initial_language_id` INT(11) NOT NULL DEFAULT '0',
-    `status` INT(11) NOT NULL DEFAULT '0',
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `content_id` INT DEFAULT NULL,
+    `version_no` INT NOT NULL DEFAULT '0',
+    `creator_id` INT NOT NULL DEFAULT '0',
+    `created` INT NOT NULL DEFAULT '0',
+    `initial_language_id` INT NOT NULL DEFAULT '0',
+    `status` INT NOT NULL DEFAULT '0',
     `fields` LONGTEXT NOT NULL,
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
@@ -150,13 +150,13 @@ CREATE TABLE ezcontentversion (
 
 DROP TABLE IF EXISTS ezcontentobject_relation; -- Formerlly ezcontentobject_link
 CREATE TABLE ezcontentobject_relation (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `contenttype_field_id` INT(11) NOT NULL DEFAULT '0',
-    `from_content_id` INT(11) NOT NULL DEFAULT '0',
-    `from_contentobject_version_no` INT(11) NOT NULL DEFAULT '0',
-    `to_content_id` INT(11) NOT NULL DEFAULT '0',
-    `op_code` INT(11) NOT NULL DEFAULT '0',
-    `relation_type` INT(11) NOT NULL DEFAULT '1',
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `contenttype_field_id` INT NOT NULL DEFAULT '0',
+    `from_content_id` INT NOT NULL DEFAULT '0',
+    `from_contentobject_version_no` INT NOT NULL DEFAULT '0',
+    `to_content_id` INT NOT NULL DEFAULT '0',
+    `op_code` INT NOT NULL DEFAULT '0',
+    `relation_type` INT NOT NULL DEFAULT '1',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (contenttype_field_id) REFERENCES ezcontenttype_field(id) ON DELETE RESTRICT,
@@ -169,20 +169,20 @@ CREATE TABLE ezcontentobject_relation (
 --
 DROP TABLE IF EXISTS ezcontentlocation;
 CREATE TABLE ezcontentlocation (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `main_id` INT(11) DEFAULT NULL,
-    `parent_id` INT(11) NOT NULL DEFAULT '0',
-    `content_id` INT(11) DEFAULT NULL,
-    `contentobject_version_no` INT(11) DEFAULT NULL,
-    `contentobject_is_published` INT(11) DEFAULT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `main_id` INT DEFAULT NULL,
+    `parent_id` INT NOT NULL DEFAULT '0',
+    `content_id` INT DEFAULT NULL,
+    `contentobject_version_no` INT DEFAULT NULL,
+    `contentobject_is_published` INT DEFAULT NULL,
     `path_string` VARCHAR(255) NOT NULL DEFAULT '',
-    `depth` INT(11) NOT NULL DEFAULT '0',
-    `priority` INT(11) NOT NULL DEFAULT '0',
+    `depth` INT NOT NULL DEFAULT '0',
+    `priority` INT NOT NULL DEFAULT '0',
     `remote_id` VARCHAR(100) NOT NULL DEFAULT '',
-    `is_hidden` INT(11) NOT NULL DEFAULT '0',
-    `is_invisible` INT(11) NOT NULL DEFAULT '0',
-    `sort_field` INT(11) DEFAULT '1',
-    `sort_order` INT(11) DEFAULT '1',
+    `is_hidden` INT NOT NULL DEFAULT '0',
+    `is_invisible` INT NOT NULL DEFAULT '0',
+    `sort_field` INT DEFAULT '1',
+    `sort_order` INT DEFAULT '1',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (main_id) REFERENCES ezcontentlocation(id) ON DELETE RESTRICT,
@@ -192,27 +192,27 @@ CREATE TABLE ezcontentlocation (
 
 DROP TABLE IF EXISTS ezcontentobject_trash;
 CREATE TABLE ezcontentobject_trash (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `main_id` INT(11) DEFAULT NULL,
-    `parent_id` INT(11) NOT NULL DEFAULT '0',
-    `content_id` INT(11) DEFAULT NULL,
-    `contentobject_version_no` INT(11) DEFAULT NULL,
-    `contentobject_is_published` INT(11) DEFAULT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `main_id` INT DEFAULT NULL,
+    `parent_id` INT NOT NULL DEFAULT '0',
+    `content_id` INT DEFAULT NULL,
+    `contentobject_version_no` INT DEFAULT NULL,
+    `contentobject_is_published` INT DEFAULT NULL,
     `path_string` VARCHAR(255) NOT NULL DEFAULT '',
-    `depth` INT(11) NOT NULL DEFAULT '0',
-    `priority` INT(11) NOT NULL DEFAULT '0',
+    `depth` INT NOT NULL DEFAULT '0',
+    `priority` INT NOT NULL DEFAULT '0',
     `remote_id` VARCHAR(100) NOT NULL DEFAULT '',
-    `is_hidden` INT(11) NOT NULL DEFAULT '0',
-    `is_invisible` INT(11) NOT NULL DEFAULT '0',
-    `sort_field` INT(11) DEFAULT '1',
-    `sort_order` INT(11) DEFAULT '1',
+    `is_hidden` INT NOT NULL DEFAULT '0',
+    `is_invisible` INT NOT NULL DEFAULT '0',
+    `sort_field` INT DEFAULT '1',
+    `sort_order` INT DEFAULT '1',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `ezsection`;
 CREATE TABLE `ezsection` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(255) DEFAULT NULL,
     `language_code` VARCHAR(255) DEFAULT NULL,
     `name` VARCHAR(255) DEFAULT NULL,
@@ -225,21 +225,21 @@ CREATE TABLE `ezsection` (
 --
 DROP TABLE IF EXISTS ezuser;
 CREATE TABLE ezuser (
-    `id` INT(11) NOT NULL DEFAULT '0',
-    `content_id` INT(11) DEFAULT NULL, -- Introduced as an optional content relation
+    `id` INT NOT NULL DEFAULT '0',
+    `content_id` INT DEFAULT NULL, -- Introduced as an optional content relation
     `email` VARCHAR(150) NOT NULL DEFAULT '',
     `login` VARCHAR(150) NOT NULL DEFAULT '',
     `password_hash` VARCHAR(50) DEFAULT NULL,
-    `password_hash_type` INT(11) NOT NULL DEFAULT '1',
+    `password_hash_type` INT NOT NULL DEFAULT '1',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS ezuser_setting;
 CREATE TABLE ezuser_setting (
-    `user_id` INT(11) NOT NULL DEFAULT '0',
-    `is_enabled` INT(11) NOT NULL DEFAULT '0',
-    `max_login` INT(11) DEFAULT NULL,
+    `user_id` INT NOT NULL DEFAULT '0',
+    `is_enabled` INT NOT NULL DEFAULT '0',
+    `max_login` INT DEFAULT NULL,
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (user_id),
     FOREIGN KEY (user_id) REFERENCES ezuser(id) ON DELETE CASCADE
@@ -247,9 +247,9 @@ CREATE TABLE ezuser_setting (
 
 DROP TABLE IF EXISTS `ezuser_accountkey`;
 CREATE TABLE `ezuser_accountkey` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `user_id` INT(11) NOT NULL DEFAULT '0',
-    `time` INT(11) NOT NULL DEFAULT '0',
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL DEFAULT '0',
+    `time` INT NOT NULL DEFAULT '0',
     `hash_key` VARCHAR(32) NOT NULL DEFAULT '',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
@@ -258,11 +258,11 @@ CREATE TABLE `ezuser_accountkey` (
 
 DROP TABLE IF EXISTS `ezuservisit`;
 CREATE TABLE `ezuservisit` (
-    `user_id` INT(11) NOT NULL DEFAULT '0',
-    `current_visit_timestamp` INT(11) NOT NULL DEFAULT '0',
-    `failed_login_attempts` INT(11) NOT NULL DEFAULT '0',
-    `last_visit_timestamp` INT(11) NOT NULL DEFAULT '0',
-    `login_count` INT(11) NOT NULL DEFAULT '0',
+    `user_id` INT NOT NULL DEFAULT '0',
+    `current_visit_timestamp` INT NOT NULL DEFAULT '0',
+    `failed_login_attempts` INT NOT NULL DEFAULT '0',
+    `last_visit_timestamp` INT NOT NULL DEFAULT '0',
+    `login_count` INT NOT NULL DEFAULT '0',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`user_id`),
     FOREIGN KEY (user_id) REFERENCES ezuser(id) ON DELETE CASCADE
@@ -270,7 +270,7 @@ CREATE TABLE `ezuservisit` (
 
 DROP TABLE IF EXISTS ezrole;
 CREATE TABLE ezrole (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `identifier` VARCHAR(255) NOT NULL DEFAULT '',
     `name` LONGTEXT,
     `description` LONGTEXT,
@@ -280,9 +280,9 @@ CREATE TABLE ezrole (
 
 DROP TABLE IF EXISTS ezrole_content_rel;
 CREATE TABLE ezrole_content_rel (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `role_id` INT(11) DEFAULT NULL,
-    `content_id` INT(11) DEFAULT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `role_id` INT DEFAULT NULL,
+    `content_id` INT DEFAULT NULL,
     `limit_identifier` VARCHAR(255) DEFAULT '',
     `limit_value` VARCHAR(255) DEFAULT '',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -293,8 +293,8 @@ CREATE TABLE ezrole_content_rel (
 
 DROP TABLE IF EXISTS ezpolicy;
 CREATE TABLE ezpolicy (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `role_id` INT(11) DEFAULT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `role_id` INT DEFAULT NULL,
     `function_name` VARCHAR(255) DEFAULT NULL,
     `module_name` VARCHAR(255) DEFAULT NULL,
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -304,8 +304,8 @@ CREATE TABLE ezpolicy (
 
 DROP TABLE IF EXISTS ezpolicy_limitation;
 CREATE TABLE ezpolicy_limitation (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `policy_id` INT(11) DEFAULT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `policy_id` INT DEFAULT NULL,
     `identifier` VARCHAR(255) NOT NULL DEFAULT '',
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
@@ -314,8 +314,8 @@ CREATE TABLE ezpolicy_limitation (
 
 DROP TABLE IF EXISTS ezpolicy_limitation_value;
 CREATE TABLE ezpolicy_limitation_value (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `limitation_id` INT(11) DEFAULT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `limitation_id` INT DEFAULT NULL,
     `value` VARCHAR(255) DEFAULT NULL,
     `changed` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
