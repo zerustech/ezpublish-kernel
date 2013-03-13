a0614e10c5251f31e83a7cf938c54573a058b157
Patrick Allaert <pa@ez.no>
2013-03-13 14:58:54 +0100

    Added: ApiLoader\SqlNgStorageEngineFactory

---
+++ eZ/Bundle/EzPublishCoreBundle/ApiLoader/SqlNgStorageEngineFactory.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * File containing the SqlNgStorageEngineFactory class.
+ *
+ * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version //autogentag//
+ */
+
+namespace eZ\Bundle\EzPublishCoreBundle\ApiLoader;
+
+use eZ\Publish\Core\Persistence\Legacy\EzcDbHandler;
+use eZ\Publish\Core\Persistence\SqlNg\Content\StorageRegistry;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class SqlNgStorageEngineFactory
+{
+    /**
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * Collection of converters with identifier as key and FQN class name as value
+     *
+     * @var array
+     */
+    protected $converters = array();
+
+    protected $fieldTypes = array();
+
+    public function __construct( ContainerInterface $container )
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Registers a field type converter as expected in sqlng storage engine.
+     * $className must implement eZ\Publish\Core\Persistence\Legacy\Content\FieldValue\Converter interface.
+     *
+     * @param string $typeIdentifier Field type identifier the converter will be used for
+     * @param string $className FQN of the converter class
+     */
+    public function registerFieldTypeConverter( $typeIdentifier, $className )
+    {
+        $this->converters[$typeIdentifier] = $className;
+    }
+
+    /**
+     * Registers an eZ Publish field type.
+     * Field types are being registered as a closure so that they will be lazy loaded.
+     *
+     * @param string $fieldTypeServiceId The field type service Id
+     * @param string $fieldTypeAlias The field type alias (e.g. "ezstring")
+     */
+    public function registerFieldType( $fieldTypeServiceId, $fieldTypeAlias )
+    {
+        $container = $this->container;
+        $this->fieldTypes[$fieldTypeAlias] = function() use ( $container, $fieldTypeServiceId )
+        {
+            return $container->get( $fieldTypeServiceId );
+        };
+    }
+
+    /**
+     * Builds the SqlNg Storage Engine
+     *
+     * @param \eZ\Publish\Core\Persistence\Legacy\EzcDbHandler $dbhandler
+     * @param boolean $deferTypeUpdate
+     *
+     * @return \eZ\Publish\Core\Persistence\SqlNg\Handler
+     */
+    public function buildEngine( EzcDbHandler $dbhandler, $deferTypeUpdate )
+    {
+        $engineClass = $this->container->getParameter( 'ezpublish.api.storage_engine.sqlng.class' );
+        return new $engineClass(
+            $dbhandler,
+            new StorageRegistry(
+                $this->container->get( 'ezpublish.api.repository.factory' )->getExternalStorageHandlers()
+            ),
+            $this->container->get( 'ezpublish.api.storage_engine.legacy.transformation_processor' ),
+            array(
+                'defer_type_update' => (bool)$deferTypeUpdate,
+                'field_type' => $this->fieldTypes,
+            )
+        );
+    }
+}
