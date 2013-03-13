ccdaaf5159320aff9ab909d9d3f2adbe22b61ed4
Kore Nordmann <mail@kore-nordmann.de>
2013-03-13 15:31:02 +0100

    Create a field value from data

--- eZ/Publish/Core/Persistence/SqlNg/Content/Type/Mapper.php
+++ eZ/Publish/Core/Persistence/SqlNg/Content/Type/Mapper.php
@@ -164,7 +164,7 @@ class Mapper
         $field->isInfoCollector = $row['ezcontenttype_field_is_information_collector'] == 1;
         $field->isSearchable = (bool)$row['ezcontenttype_field_is_searchable'];
         $field->position = (int)$row['ezcontenttype_field_placement'];
-        $field->defaultValue = json_decode( $row['ezcontenttype_field_default_value'], true );
+        $field->defaultValue = new Persistence\Content\FieldValue( json_decode( $row['ezcontenttype_field_default_value'], true ) );
         $field->fieldTypeConstraints = json_decode( $row['ezcontenttype_field_constraints'], true );
 
         return $field;
